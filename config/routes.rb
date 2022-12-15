d7947a890f0c6d8cb03928300e04db01739451fd
menanya <emmymenz@gmail.com>
2022-12-15 22:51:03 +0100

    create controllers

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,18 @@
 Rails.application.routes.draw do
+  resources :payment_shows
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
+ 
+  namespace :api do
+    namespace :v1 do
+      resources :users
+      resources :products
+      resources :addresses
+      resources :cart_items 
+      resources :product_categories 
+
+      end
+    end
+
 
   # Defines the root path route ("/")
   # root "articles#index"
