72ea4250c55278ccd20c54d26028c46747a80675
morris menanya <emmiemenz@gmail.com>
2023-04-04 12:02:13 +0100

    update product

--- db/migrate/20230312142216_add_gender_reference_to_products.rb
+++ db/migrate/20230312142216_add_gender_reference_to_products.rb
@@ -1,7 +1,7 @@
 class AddGenderReferenceToProducts < ActiveRecord::Migration[7.0]
   def change
-    # add_reference :products, :gender, null: false, foreign_key: true
-    add_reference :products, :gender, foreign_key: true
+    add_reference :products, :gender, null: false, foreign_key: true
+    # add_reference :products, :gender, foreign_key: true
 
   end
 end
