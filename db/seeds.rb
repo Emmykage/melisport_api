2f5efc60f9023b88d87f9035f92946949c16bd4d
morris menanya <emmiemenz@gmail.com>
2024-06-01 08:22:25 +0100

    clear write up comment from seed file

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,13 +1,3 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
-#   Character.create(name: "Luke", movie: movies.first)
-
-
-
 user_one = User.create(first_name: "Morris", last_name: "Menanya", phone_no: '07064334160', email: "emmiemenz@gmail.com", role: "admin", password: "chemistry101" )
 
 genders = Gender.create([{name: "men"}, {name: "women"}])
