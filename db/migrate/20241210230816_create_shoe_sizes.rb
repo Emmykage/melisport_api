5c0aa943dfa23a10768394e76f015a5098ac56d7
morris menanya <emmiemenz@gmail.com>
2024-12-16 09:33:45 +0100

    fix migration

--- db/migrate/20241210230816_create_shoe_sizes.rb
+++ db/migrate/20241210230816_create_shoe_sizes.rb
@@ -1,7 +1,7 @@
 class CreateShoeSizes < ActiveRecord::Migration[7.0]
   def change
-    create_table :shoe_sizes, id: false uid do |t|
-      t.string :size
+    create_table :shoe_sizes, id: false  do |t|
+      t.string :size, null: false
       t.integer :quantity, default: 0
       t.references :product, foreign_key: true, type: :uuid
 
