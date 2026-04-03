025e517a4e14fed2f8d7a50d13db5eb3fa6ce77e
morris menanya <emmiemenz@gmail.com>
2026-04-03 16:08:21 +0100

    include the mail

--- app/mailers/application_mailer.rb
+++ app/mailers/application_mailer.rb
@@ -1,5 +1,5 @@
 class ApplicationMailer < ActionMailer::Base
   # default from: 'from@example.com'
-  default from: 'support@melisports.com'
+  default from: 'Melisports <info@melisports.com>'
   layout 'mailer'
 end
--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -1,5 +1,5 @@
 class UserMailer < ApplicationMailer
-  default from: 'info@melisports.com'
+  # default from: 'info@melisports.com'
 
   def confirmation_email(user)
     # @user = params[:user]
